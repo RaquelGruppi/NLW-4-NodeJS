bf07631fe88346e9cf550ae7ea54909c09ebb5b1
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-02-26 17:16:16 +1100

    4th day

--- routes.ts
+++ routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './src/controllers/SendMailController';
 import { SurveyController } from './src/controllers/SurveyController';
 import { UserController } from './src/controllers/UserController';
 
@@ -6,9 +7,12 @@ const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
